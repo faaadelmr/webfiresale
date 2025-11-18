0833ba6639bd111b45b929a11d6ae84345e08588
411221027 <411221027@mahasiswa.undira.ac.id>
2025-11-18 07:50:25 +0000

    update local storage

---
+++ prisma/migrations/20251118074645_webfiresale/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "provider" TEXT,
+    "providerId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
